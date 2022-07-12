624a1e4da384d1ce5e39368c9a89c4d2e0f6c58e
unknown <miracledev0303@gmail.com>
2022-07-12 14:22:31 -0300

    Websocket Server Auth

--- server/resolvers.js
+++ server/resolvers.js
@@ -30,7 +30,11 @@ export const resolvers = {
 
   Subscription: {
     messageAdded: {
-      subscribe: () => pubSub.asyncIterator('MESSAGE_ADDED')
+      subscribe: (_root, _args, { userId }) => {
+        rejectIf(!userId);
+        
+        return pubSub.asyncIterator('MESSAGE_ADDED')
+      }
     }
   }
 };
